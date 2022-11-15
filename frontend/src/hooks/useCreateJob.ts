f5e4e8903961319a1f1dfd9758ff17dc9ad92ea7
Jan Kondratowicz <jan@getbits.app>
2022-11-15 21:32:21 +0100

    feat: create job hook

---
+++ frontend/src/hooks/useCreateJob.ts
@@ -0,0 +1,139 @@
+import {
+  useContractWrite,
+  usePrepareContractWrite,
+  useWaitForTransaction,
+} from "wagmi";
+import { useEffect, useState } from "react";
+import { registryAddress } from "../utils";
+import UmpireRegistry from "../artifacts/contracts/UmpireRegistry.sol/UmpireRegistry.json";
+import { infixToPostfix } from "../utils/formulas";
+import {
+  comparatorToString,
+  postfixNodeToTuple,
+  PostfixNodeTuple,
+  UmpireComparator,
+} from "../utils/model";
+
+export interface IUseCreateJobProps {
+  jobName: string;
+  leftFormula: string;
+  comparator: UmpireComparator;
+  rightFormula: string;
+  variableFeeds: string[];
+  activationTimestamp: number;
+  timeoutTimestamp: number;
+  actionAddress: string;
+}
+
+export interface IUseCreateJob {
+  isLeftValid: boolean;
+  isRightValid: boolean;
+  isLoading: boolean;
+  isSuccess: boolean;
+  transactionHash: string | undefined;
+  deployJob: () => Promise<void>;
+  error: Error | null;
+  isError: boolean;
+  prepareError: Error | null;
+  isPrepareError: boolean;
+  fullFormula: string;
+}
+
+export const useCreateJob = ({
+  jobName,
+  leftFormula,
+  comparator,
+  rightFormula,
+  variableFeeds,
+  activationTimestamp,
+  timeoutTimestamp,
+  actionAddress,
+}: IUseCreateJobProps): IUseCreateJob => {
+  const [isLeftValid, setLeftValid] = useState<boolean>(false);
+  const [isRightValid, setRightValid] = useState<boolean>(false);
+
+  const [leftAsTuples, setLeftAsTuples] = useState<PostfixNodeTuple[]>([]);
+  const [rightAsTuples, setRightAsTuples] = useState<PostfixNodeTuple[]>([]);
+
+  const [fullFormula, setFullFormula] = useState<string>("");
+
+  useEffect(() => {
+    try {
+      const left = infixToPostfix(leftFormula);
+      setLeftAsTuples(left.map(postfixNodeToTuple));
+      setLeftValid(true);
+    } catch (e: any) {
+      console.log(e);
+      setLeftValid(false);
+    }
+
+    try {
+      const right = infixToPostfix(rightFormula);
+      setRightAsTuples(right.map(postfixNodeToTuple));
+      setRightValid(true);
+    } catch (e: any) {
+      console.log(e);
+      setLeftValid(false);
+    }
+
+    setFullFormula(
+      `${leftFormula} ${comparatorToString(comparator)} ${rightFormula}`
+    );
+  }, [leftFormula, rightFormula]);
+
+  const {
+    config,
+    error: prepareError,
+    isError: isPrepareError,
+  } = usePrepareContractWrite({
+    address: registryAddress,
+    abi: UmpireRegistry.abi,
+    functionName: "createJobFromNodes",
+    enabled: isLeftValid && isRightValid,
+    args: [
+      jobName,
+      leftAsTuples,
+      comparator,
+      rightAsTuples,
+      variableFeeds,
+      activationTimestamp,
+      timeoutTimestamp,
+      actionAddress,
+    ],
+  });
+
+  const {
+    data,
+    error,
+    isError,
+    write,
+    // @ts-ignore
+  } = useContractWrite(config);
+
+  const { isLoading, isSuccess } = useWaitForTransaction({
+    hash: data?.hash,
+  });
+
+  const deployJob = async () => {
+    if (!isLeftValid || !isRightValid) {
+      console.error("Invalid formulas");
+      return;
+    }
+
+    await write?.();
+  };
+
+  return {
+    isLeftValid,
+    isRightValid,
+    isLoading,
+    isSuccess,
+    transactionHash: data?.hash,
+    deployJob,
+    error,
+    isError,
+    prepareError,
+    isPrepareError,
+    fullFormula,
+  };
+};
